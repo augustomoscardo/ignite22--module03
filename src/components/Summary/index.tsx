94100d9aabb7464ac74dd5efd19fb11ea8f591eb
Augusto Moscardo <augustomsaluz@gmail.com>
2023-05-01 23:29:52 -0300

    first commit

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,59 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
+import { useContext } from 'react';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { SummaryCard, SummaryContainer } from "./styles";
+
+export function Summary() {
+  const { transactions } = useContext(TransactionsContext)
+
+  const summary = transactions.reduce(
+    (accumulator, transaction) => {
+      if (transaction.type === 'income') {
+        accumulator.income += transaction.price
+        accumulator.total += transaction.price
+      } else {
+        accumulator.outcome += transaction.price
+        accumulator.total -= transaction.price
+      }
+
+      return accumulator
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0
+    }
+  )
+
+
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color='#00b37e' />
+        </header>
+
+        <strong>{summary.income}</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color='#f75a68' />
+        </header>
+
+        <strong>{summary.outcome}</strong>
+      </SummaryCard>
+
+      <SummaryCard variant='green'>
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color='#fff' />
+        </header>
+
+        <strong>{summary.total}</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+
+interface Transaction {
+  id: string
+  description: string
+  type: 'income' | 'outcome'
+  price: number
+  category: string
+  createdAt: string
+}
+
+export function Transactions() {
+  const { transactions } = useContext(TransactionsContext)
+
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+
+        <TransactionsTable>
+          <tbody>
+            {transactions.map(transaction => (
+              <tr key={transaction.id}>
+                <td width='50%'>{transaction.description}</td>
+                <td>
+                  <PriceHighLight variant={transaction.type}>
+                    {transaction.price}
+                  </PriceHighLight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            ))}
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
+}
